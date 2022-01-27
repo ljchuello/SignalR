cc7562d2eb1785335f924df3a86e3c18ef9d906e
Leonardo Chuello <ljchuello@gmail.com>
2022-01-26 21:01:22 -0500

    Ready!.

--- Telegrama/Server/Program.cs
+++ Telegrama/Server/Program.cs
@@ -1,13 +1,32 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Telegrama.Server.Hub;
+
+string cors = "LJChuello";
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSignalR();
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddResponseCompression(x =>
+{
+    x.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+        new[] { "application/octet-stream" }
+    );
+});
+builder.Services.AddCors(x =>
+{
+    x.AddPolicy(cors, y =>
+    {
+        y.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
+
+app.UseResponseCompression();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -28,9 +47,11 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseCors(cors);
 
 app.MapRazorPages();
 app.MapControllers();
+app.MapHub<ChatHub>("/chathub");
 app.MapFallbackToFile("index.html");
 
 app.Run();
